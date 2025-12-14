085eb4e0cf8da3224dfd01c63b252a1039475e8d
unknown <mdtamimhossain1357@gmail.com>
2025-12-14 12:31:13 +0600

    Implement hrRegister, employeeRegister, and login controller functions.

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use('/api/v1/auth', authRoutes);
-
 // Database Connection
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected successfully.'))
