f36da3effa6c5d990dd4f7acaabc5db629b86e20
unknown <mdtamimhossain1357@gmail.com>
2025-12-22 08:31:11 +0600

    add verifyToken and verifyHR security middlewares

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
-const uri = process.env.MONGO_URI;
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
   serverApi: {
